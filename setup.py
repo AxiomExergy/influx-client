855dd7a42808b796f344e01674bcec17d9c44ef3
Jacob Alheid <shakefu@gmail.com>
2018-04-02 10:43:36 -0700

    setup: Get long description in place.

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import setup, find_packages
 
 
+def readme():
+    try:
+        with open('README.md', 'r') as doc:
+            return doc.read()
+    except IOError:
+        return """\
+# influx-client
+
+See [GitHub](https://github.com/AxiomExergy/influx-client) for documentation.
+"""
+
+
 # Used for installing test dependencies directly
 tests_require = [
     'mock',
@@ -12,6 +24,10 @@ setup(
     name='influx-client',
     version='1.0.0',
     description="InfluxDB client",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    license="Apache License v2.0",
+    platforms=['any'],
     author="Jacob Alheid",
     author_email="shakefu@gmail.com",
     packages=find_packages(exclude=['test', 'test_*', 'fixtures']),
