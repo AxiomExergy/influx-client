bf3cc1dae957c531569548741d11b4faa4d2645e
Jacob Alheid <shakefu@gmail.com>
2018-04-02 11:11:34 -0700

    setup: Bump to 1.0.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ tests_require = [
 
 setup(
     name='influx-client',
-    version='1.0.1',
+    version='1.0.2',
     description="InfluxDB client",
     long_description=readme(),
     long_description_content_type="text/markdown",
