3b901e57b27363a5d9e713bcc01f3b54a29c78b4
Jacob Alheid <shakefu@gmail.com>
2018-04-27 13:07:51 -0700

    influx: Add unpack method, fix select_where limit.

--- influx/__init__.py
+++ influx/__init__.py
@@ -132,6 +132,62 @@ class InfluxDB:
         if resp.status_code != 204:
             return resp.json()
 
+    def unpack(self, result):
+        """
+        Return the column and values keys from *result*, expecting one series.
+
+        :param dict result: Result dictionary as returned by API
+        :return tuple: 2-tuple of columns and values
+
+        Expects an object like:
+
+            {'results': [{
+                'statement_id': 0,
+                'series': [{
+                    'name': <measurement>,
+                    'columns': [ ... ],
+                    'values': [[ ... ], ...]
+                    }]
+                }]
+            }
+
+        """
+        def _debug(*args):
+            # print(*args)
+            pass
+
+        _debug("Result", result)
+
+        empty = [], []
+        result = result.get('results', None)
+        if not result or not len(result):
+            _debug("No results key or empty", result)
+            return empty
+
+        result = result[0]
+        if not result or not isinstance(result, dict):
+            _debug("No first statement or not dict", result)
+            return empty
+
+        result = result.get('series', None)
+        if not result or not len(result):
+            _debug("No series or series empty", result)
+            return empty
+
+        result = result[0]
+        if not result or not isinstance(result, dict):
+            _debug("Series empty or not dict", result)
+            return empty
+
+        columns = result.get('columns', None)
+        if not columns or not len(columns):
+            _debug("No columns or columns empty", columns)
+            return empty
+
+        values = result.get('values', [])
+
+        return columns, values
+
     def select_recent(self, database, measurement, fields='*', tags=None,
                       relative_time="15m"):
         """
@@ -194,17 +250,13 @@ class InfluxDB:
         if tags:
             where += " AND {}".format(InfluxDB._format_tags(tags))
 
-        kwargs = dict(
-            database=database,
-            measurement=measurement,
-            fields=fields,
-            where=where,
-            )
-
+        # Add the limit into the WHERE clause so it's ordered correctly
         if limit:
-            kwargs['limit'] = limit
+            where += " LIMIT {}".format(limit)
 
-        resp = self._safe_request(IQL_SELECT, **kwargs)
+        resp = self._safe_request(IQL_SELECT, database=database,
+                                  measurement=measurement, fields=fields,
+                                  where=where)
         InfluxDB._check_and_raise(resp)
         return resp.json()
 
