57850e2f36af0fa86b66ca95bd2b0da0dbab864e
Jacob Alheid <shakefu@gmail.com>
2018-10-03 14:41:15 -0700

    setup: Version 1.7.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require = [
 
 setup(
     name='influx-client',
-    version='1.7.0',
+    version='1.7.1',
     description="InfluxDB client",
     long_description=readme(),
     long_description_content_type="text/markdown",
